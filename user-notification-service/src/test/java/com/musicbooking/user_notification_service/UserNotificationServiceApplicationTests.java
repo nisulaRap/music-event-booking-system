e75cc98f6d6683f0d141613462023f10d938fe08
nisulaRap <nisula.rap@gmail.com>
2026-03-03 11:15:56 +0530

    Initial commit

---
+++ user-notification-service/src/test/java/com/musicbooking/user_notification_service/UserNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicbooking.user_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
