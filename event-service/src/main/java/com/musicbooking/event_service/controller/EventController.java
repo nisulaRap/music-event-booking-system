622bedb73ebfad6e2d258341d1b8e8682ca4d0ef
nisulaRap <nisula.rap@gmail.com>
2026-03-05 10:35:56 +0530

    added EventController class for managing event-related operations

---
+++ event-service/src/main/java/com/musicbooking/event_service/controller/EventController.java
@@ -0,0 +1,59 @@
+package com.musicbooking.event_service.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.musicbooking.event_service.dto.EventRequest;
+import com.musicbooking.event_service.model.Event;
+import com.musicbooking.event_service.service.EventService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService service;
+
+    @PostMapping
+    public Event create(@Valid @RequestBody EventRequest request) {
+        return service.createEvent(request);
+    }
+
+    @GetMapping
+    public List<Event> getAll() {
+        return service.getAllEvents();
+    }
+
+    @GetMapping("/{id}")
+    public Event getById(@PathVariable String id) {
+        return service.getEventById(id);
+    }
+    
+    @PutMapping("/{id}")
+    public Event update(@PathVariable String id, @Valid @RequestBody EventRequest request) {
+        return service.updateEvent(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public Event delete(@PathVariable String id) {
+        service.deleteEvent(id);
+        return null;
+    }
+    
+    // Integration with Booking Service to reduce available seats
+    @PutMapping("/{id}/reduce-seats")
+    public Event reduceSeats(@PathVariable String id, @RequestBody int seats) {
+        return service.reduceSeats(id, seats);
+    }
+}
